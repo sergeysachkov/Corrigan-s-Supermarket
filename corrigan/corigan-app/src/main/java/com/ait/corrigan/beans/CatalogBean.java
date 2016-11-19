e04269a994ad3a82f81036293f3f46161d623509
AdrianDI <a00200608@student.ait.ie>
2016-11-19 23:12:52 +0000

    Implement getItem, getAllItems. Add tests for getItem, getAllItems

---
+++ corrigan/corigan-app/src/main/java/com/ait/corrigan/beans/CatalogBean.java
@@ -0,0 +1,5 @@
+package com.ait.corrigan.beans;
+
+public class CatalogBean {
+
+}
--- corrigan/corrigan-impl/src/main/java/com/ait/corrigan/dao/ItemDaoImpl.java
+++ corrigan/corrigan-impl/src/main/java/com/ait/corrigan/dao/ItemDaoImpl.java
@@ -2,6 +2,11 @@ package com.ait.corrigan.dao;
 
 import com.ait.corrigan.models.shop.Item;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDaoImpl implements ItemDao {
@@ -18,17 +23,58 @@ public class ItemDaoImpl implements ItemDao {
 		return 0;
 	}
 
+	//=======ADRIAN=======
 	public List<Item> getAllItems() {
-		return null;
+		List<Item> allItems=new ArrayList<Item>();
+		Item item=null;
+		
+		try {
+			Connection con = DaoUtil.getConnection();
+
+			String sql = "SELECT * FROM ITEMS";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				item = new Item(rs.getLong(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5),
+						rs.getInt(6));
+				allItems.add(item);
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return allItems;
+	
 	}
 
 	public List<Item> getItemsByCategory(String category) {
 		return null;
 	}
 
+	// ========Adrian========
 	@Override
 	public Item getItem(long itemId) {
-		// TODO Auto-generated method stub
-		return null;
+		Item item=null;
+		try {
+			Connection con = DaoUtil.getConnection();
+
+			String sql = "SELECT * FROM ITEMS WHERE itemID=?";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setLong(1, itemId);
+			ResultSet rs = pstmt.executeQuery();
+
+			if (!rs.next()) {
+				return null;
+			}
+
+			 item = new Item(rs.getLong(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5),
+					rs.getInt(6));
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return item;
 	}
 }
