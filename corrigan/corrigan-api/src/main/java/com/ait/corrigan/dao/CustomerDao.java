23d3b9ec700100e091b1ddc84079832fbce0f530
John <John@dell>
2016-11-22 15:52:45 +0000

    change

--- corrigan/corrigan-api/src/main/java/com/ait/corrigan/dao/CustomerDao.java
+++ corrigan/corrigan-api/src/main/java/com/ait/corrigan/dao/CustomerDao.java
@@ -1,14 +1,14 @@
 package com.ait.corrigan.dao;
 
 import com.ait.corrigan.models.user.Customer;
-import com.ait.corrigan.models.user.PaymentDetails;
+
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface CustomerDao {
 
-    long addCustomer(Customer customer);
+    long addCustomer(long customerID, Customer customer);
     long updateCustomer(Customer customer);
     void deleteCustomer(long customerId);
     Customer getCustomer(long customerId) throws SQLException;
