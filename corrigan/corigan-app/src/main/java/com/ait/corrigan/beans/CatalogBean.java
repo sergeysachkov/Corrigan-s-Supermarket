3903e31d4ed8166c1ce520989ca4a98899c9ffd6
AdrianDI <a00200608@student.ait.ie>
2016-11-24 09:32:02 +0000

    bean and model for categories

--- corrigan/corigan-app/src/main/java/com/ait/corrigan/beans/CatalogBean.java
+++ corrigan/corigan-app/src/main/java/com/ait/corrigan/beans/CatalogBean.java
@@ -7,7 +7,9 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 
+import com.ait.corrigan.models.shop.Category;
 import com.ait.corrigan.models.shop.Item;
+import com.ait.corrigan.services.CategoryServicesImpl;
 import com.ait.corrigan.services.ItemServiceImpl;
 
 @ManagedBean(name="catalog")
@@ -17,12 +19,18 @@ public class CatalogBean {
 	
 	@ManagedProperty(value="#{items}")
 	private List<Item> items=null;
+	
+	@ManagedProperty(value="#{categories}")
+	private List<Category> categories=null;
+	
+	@ManagedProperty(value="#{name}")
+	private String name="all";
 
 	 
 	 @PostConstruct
 	 public void getAllItems(){
 		 ItemServiceImpl c=new ItemServiceImpl();
-		 items= c.getCatalogue();
+		 this.items= c.getCatalogue();
 	 }
 
 	public List<Item> getItems() {
@@ -32,4 +40,31 @@ public class CatalogBean {
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
+	
+	 @PostConstruct
+	public void getAllCat(){
+		CategoryServicesImpl c= new CategoryServicesImpl();
+		this.categories=c.getAllCategories();
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	 
 }
