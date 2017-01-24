10e56252c86d808cd21bc76541a6412bfe4220de
Joshua <iyezi@hotmail.com>
2017-01-24 22:42:41 +0000

    Changed the signature of Category dao: passing primitives instead of the model Implemented getNewID(), addCategory() method of Category dao.

--- corrigan/corrigan-impl/src/main/java/com/ait/corrigan/dao/CategoryDAOImpl.java
+++ corrigan/corrigan-impl/src/main/java/com/ait/corrigan/dao/CategoryDAOImpl.java
@@ -8,55 +8,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ait.corrigan.models.shop.Category;
-import com.ait.corrigan.models.shop.Item;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CategoryDAOImpl implements CategoryDao {
 
-	@Override
-	public void addCategory(Category category) throws SQLException {
-		// TODO Auto-generated method stub
+    @Override
+    public long getNewId() throws SQLException {
+        String sql = "SELECT cateID FROM Categories ORDER BY cateID DESC LIMIT 1";
+        Connection con = DaoUtil.getConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        if (rs.next()) {
+            long id=rs.getLong(1);
+            con.close();
+            return ++id;
+        } else {
+            con.close();
+            return 1;
+        }
+    }
 
-	}
+    @Override
+    public List<Category> getAllCategories() throws SQLException {
 
-	@Override
-	public void deleteCategory(int categoryId) throws SQLException {
-		// TODO Auto-generated method stub
+        List<Category> allCategories = new ArrayList<Category>();
+        Category cat = null;
+        Connection con = null;
+        try {
+            con = DaoUtil.getConnection();
+            String sql = "select cate_name from Categories";
 
-	}
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                cat = new Category(rs.getString(1));
+                allCategories.add(cat);
+            }
 
-	@Override
-	public void updateCategory(Category category) throws SQLException {
-		// TODO Auto-generated method stub
+        } catch (SQLException e) {
 
-	}
+            e.printStackTrace();
+        } finally {
+            con.close();
+        }
+        return allCategories;
 
-	public Category getCategory(int categoryId) throws SQLException {
-		return null;
-	}
-	
-	@Override
-	public List<Category> getAllCategories() throws SQLException {
-		
-		 List<Category> allCategories = new ArrayList<Category>();
-		 Category cat = null;
+    }
 
-	        try {
-	            Connection con = DaoUtil.getConnection();
-	            String sql = "select cate_name from Categories";
-	           
-	            PreparedStatement pstmt = con.prepareStatement(sql);
-	            ResultSet rs = pstmt.executeQuery();
-	            while (rs.next()) {
-	            	cat = new Category(rs.getString(1));
-	            	allCategories.add(cat);
-	            }
 
-	        } catch (SQLException e) {
+    @Override
+    public void addCategory(long id, String name) throws SQLException {
+        String sql = "INSERT INTO Categories (cateID, cate_name) VALUES (?,?)";
+        Connection con=DaoUtil.getConnection();
+        PreparedStatement pst=con.prepareStatement(sql);
+        pst.setLong(1, id);
+        pst.setString(2, name);
+        pst.executeUpdate();
+        con.close();
+    }
 
-	            e.printStackTrace();
-	        }
-	        return allCategories;
+    @Override
+    public void deleteCategory(int categoryId) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-	}
+    @Override
+    public void updateCategory(long id, String name) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Category getCategory(int categoryId) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
